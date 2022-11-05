212df96cb1601533eeeecd5f8d3344774a934151
david-palm <david.palm1996@gmail.com>
2022-11-05 20:33:32 +0100

    Add padding to approximation

--- app/src/main/cpp/imageProcessing.cpp
+++ app/src/main/cpp/imageProcessing.cpp
@@ -34,7 +34,7 @@ void cutSudoku(cv::Mat& input, cv::Mat& output, bool padding, int kernelSize)
     auto getSudokuContour = [&](std::vector<cv::Point>& approximation)
     {
         /* Helper function to sort contours by largest area */
-        auto sortBySmallestArea = [](std::vector<cv::Point> contour1, std::vector<cv::Point> contour2)
+        auto sortBySmallestArea = [](std::vector<cv::Point> contour1, std::vector<cv::Point> contour2) -> bool
         {
             return cv::contourArea(contour1) > cv::contourArea(contour2);
         };
@@ -55,28 +55,66 @@ void cutSudoku(cv::Mat& input, cv::Mat& output, bool padding, int kernelSize)
                              0.08 * cv::arcLength(contour, true), true);
             if(approximation.size() == 4) break;
         }
-        //Displaying approximated contour
-        cv::drawContours(input, std::vector<std::vector<cv::Point>> { approximation },
-                         0, cv::Scalar(0,255,0),25);
+
     };
 
     /* Adds padding to the contour to ensure that the sudoku is not cut off on warped pages */
     auto addPadding = [&](std::vector<cv::Point>& approximation, std::vector<cv::Point>& paddedApproximation)
     {
-        auto calculateDistance = [] (cv::Point point1, cv::Point point2) -> double
+        auto calculateDistance = [] (cv::Point& point1, cv::Point& point2) -> double
         {
             int aSquared = (point1.x - point2.x) * (point1.x - point2.x);
             int bSquared = (point1.y - point2.y) * (point1.y - point2.y);
             return sqrt(aSquared + bSquared);
         };
 
+        /* Sorts the four points of the approximation (top right, top left, bottom left, bottom right) */
+        auto sortPoints = [] (std::vector<cv::Point>& points, std::vector<cv::Point>& sortedPoints)
+        {
+            auto sortByXCoordinate = [] (cv::Point& point1, cv::Point& point2) -> bool
+            { return point1.x > point2.x; };
+            auto sortByYCoordinate = [] (cv::Point& point1, cv::Point& point2) -> bool
+            { return point1.y < point2.y; };
 
+            sortedPoints.insert(sortedPoints.begin(), points.begin(), points.end());
+            //Points are first sorted by Y coordinate
+            std::sort(sortedPoints.begin(), sortedPoints.end(), sortByYCoordinate);
+            //Then the top two and bottom two points are sorted by X coordinate
+            std::sort(sortedPoints.begin(), sortedPoints.begin() + 2, sortByXCoordinate);
+            std::sort(sortedPoints.end() - 1, sortedPoints.end(), sortByXCoordinate);
+        };
+
+        double distance = calculateDistance(approximation[0], approximation[3]);
+        double cellSize = distance / 9;
+
+        sortPoints(approximation, paddedApproximation);
+
+        //Adding padding in proportion to size of sudoku cells
+        paddedApproximation[0].x += (int) (cellSize * 0.2);
+        paddedApproximation[1].x -= (int) (cellSize * 0.1);
+        paddedApproximation[2].x -= (int) (cellSize * 0.1);
+        paddedApproximation[3].x += (int) (cellSize * 0.2);
+
+        paddedApproximation[0].y -= (int) (cellSize * 0.3);
+        paddedApproximation[1].y -= (int) (cellSize * 0.3);
+        paddedApproximation[2].y += (int) (cellSize * 0.37);
+        paddedApproximation[3].y += (int) (cellSize * 0.37);
     };
 
     std::vector<cv::Point> approximation;
+    std::vector<cv::Point> paddedApproximation;
 
     prepareImage();
     getSudokuContour(approximation);
+    addPadding(approximation, paddedApproximation);
+
+    //Displaying approximated contour
+    cv::drawContours(input, std::vector<std::vector<cv::Point>> { approximation },
+                     0, cv::Scalar(0,255,0),25);
+
+    //Displaying approximated contour
+    cv::drawContours(input, std::vector<std::vector<cv::Point>> { paddedApproximation },
+                     0, cv::Scalar(0,0,255),25);
 
 
 }
