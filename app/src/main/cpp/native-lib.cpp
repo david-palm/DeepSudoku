a23343f226544428e0882be947e578b2ab49fa28
david-palm <david.palm1996@gmail.com>
2022-12-27 01:31:14 +0100

    Add frugally-deep library and dependencies (linux paths)

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,6 +7,8 @@
 #include "utils.h"
 #include "imageProcessing.h"
 
+#include <fdeep/fdeep.hpp>
+
 /* Identify sudoku returns an image with the sudoku contour highlighted in green and an array with
  * the coordinates of the sudoku contour. */
 extern "C"
@@ -60,15 +62,15 @@ Java_com_example_deepsudoku_ImageViewFragment_solveSudoku(JNIEnv *env, jobject t
     cv::Mat* digits[81];
     cutDigits(cells, digits);
     __android_log_print(ANDROID_LOG_ERROR, "cutDigits", "digits(3)[%d][%d]", (*digits[2]).size().width, (*digits[2]).size().height);
-    for(int col = 0; col < (*digits[2]).size().width; col++)
-    {
-        for(int row = 0; row < (*digits[2]).size().height; row++)
-        {
+    for(int col = 0; col < (*digits[2]).size().width; col++) {
+        for (int row = 0; row < (*digits[2]).size().height; row++) {
             __android_log_print(ANDROID_LOG_ERROR, "cutDigits", "Test [%d][%d]", col, row);
             outputMatrix.at<uint32_t>(row, col) = (*digits[2]).at<uint8_t>(row, col);
             __android_log_print(ANDROID_LOG_ERROR, "cutDigits", "Bug");
         }
     }
+
+    const auto model = fdeep::load_model("fdeep_model.json");
     __android_log_print(ANDROID_LOG_ERROR, "cutDigits", "Test 3");
     cv::circle(outputMatrix, cv::Point2i((*digits[2]).size().width, (*digits[2]).size().height), 4, cv::Scalar(255, 0, 0), -1);
     matToBitmap(env, outputMatrix, outputBitmap , false);
