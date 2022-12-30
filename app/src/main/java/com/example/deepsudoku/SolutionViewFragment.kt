e6112bda204ec66aade298abc0f6cf64a3f23cc6
david-palm <david.palm1996@gmail.com>
2022-12-30 19:26:12 +0100

    Display dummy sudoku

--- app/src/main/java/com/example/deepsudoku/SolutionViewFragment.kt
+++ app/src/main/java/com/example/deepsudoku/SolutionViewFragment.kt
@@ -3,13 +3,26 @@ package com.example.deepsudoku
 import android.content.res.AssetManager
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.example.deepsudoku.databinding.FragmentImageViewBinding
+import com.example.deepsudoku.databinding.FragmentSolutionViewBinding
 
 class SolutionViewFragment : Fragment() {
-    private var _viewBinding: FragmentImageViewBinding? = null
-    private val viewBinding: FragmentImageViewBinding get() = _viewBinding!!
-    private var imageUri: Uri? = null
-    lateinit var assetManager : AssetManager
-    lateinit var image: Bitmap
+    private var _viewBinding: FragmentSolutionViewBinding? = null
+    private val viewBinding: FragmentSolutionViewBinding get() = _viewBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _viewBinding = FragmentSolutionViewBinding.inflate(inflater, container, false)
+
+        viewBinding.cell00.text = "5";
+
+        return viewBinding.root
+    }
 }
