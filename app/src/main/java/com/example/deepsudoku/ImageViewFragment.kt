64cb3d82881df8a66fa56a02620e55b68f3fece9
david-palm <david.palm1996@gmail.com>
2022-12-30 20:29:52 +0100

    Clean up ImageViewFragment.kt

--- app/src/main/java/com/example/deepsudoku/ImageViewFragment.kt
+++ app/src/main/java/com/example/deepsudoku/ImageViewFragment.kt
@@ -1,26 +1,17 @@
 package com.example.deepsudoku
 
-import android.content.ContentResolver
-import android.content.Context
 import android.content.res.AssetManager
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.provider.MediaStore.Images.Media.getBitmap
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import androidx.core.net.toFile
 import androidx.navigation.Navigation
 import com.example.deepsudoku.databinding.FragmentImageViewBinding
-import java.io.File
-import java.net.URI
+
 
 
 class ImageViewFragment : Fragment() {
@@ -31,17 +22,13 @@ class ImageViewFragment : Fragment() {
     lateinit var image: Bitmap
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _viewBinding = FragmentImageViewBinding.inflate(inflater, container, false)
+        assetManager = resources.assets
 
         //Displaying image
         imageUri = arguments?.getParcelable<Uri>("ImageUri")
         viewBinding.imageView.setImageURI(imageUri)
-
-        assetManager = resources.assets
 
         //Set button listeners
         viewBinding.imageAcceptButton.setOnClickListener { solveSudoku() }
@@ -67,23 +54,15 @@ class ImageViewFragment : Fragment() {
         var output: Bitmap = image.copy(image.config, true)
         //Process image by calling native code
         solveSudoku(assetManager, image, output)
-        //Process image by calling native code
-        //solveSudoku(assetManager, image, output)
         viewBinding.imageView.setImageBitmap(output)
-        Navigation.findNavController(requireView()).navigate(
-            R.id.action_imageViewFragment_to_solutionViewFragment, Bundle())
+        Navigation.findNavController(requireView()).navigate(R.id.action_imageViewFragment_to_solutionViewFragment, Bundle())
     }
 
     private fun deleteImage(){
         requireContext().contentResolver.delete(imageUri!!, null, null)
-        Navigation.findNavController(requireView()).navigate(
-            R.id.action_imageViewFragment_to_imageCaptureFragment, Bundle())
+        Navigation.findNavController(requireView()).navigate(R.id.action_imageViewFragment_to_imageCaptureFragment, Bundle())
     }
 
     external fun identifySudoku(inputImage: Bitmap, outputImage: Bitmap)
     external fun solveSudoku(assetManager : AssetManager, inputImage: Bitmap, outputImage: Bitmap)
-
-    companion object {
-
-    }
 }
